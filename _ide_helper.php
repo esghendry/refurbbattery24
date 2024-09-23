81ac513a53c96a6c4793ec685af5be6c9574b6ae
Giovanni M <g.mierop@esg.works>
2024-09-23 17:04:16 +0200

    try to fix livewire js file not found

--- _ide_helper.php
+++ _ide_helper.php
@@ -22680,6 +22680,11 @@ namespace  {
     }
 
 
+namespace Facades\Livewire\Features\SupportFileUploads {
+    /**
+     * @mixin \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl     */
+    class GenerateSignedUploadUrl extends \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl {}
+}
 
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,7 @@
             href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap"
             rel="stylesheet"
         />
+        @livewireScripts
     </head>
 
     <body class="flex min-h-screen w-full flex-col font-mulish">
