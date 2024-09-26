1c6b607c0f149534c3f60079c7be24b98c50a735
Giovanni M <g.mierop@esg.works>
2024-09-26 15:07:01 +0200

    added tiny editor for filament. added ArticleResource (wip).

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -15,11 +15,33 @@ namespace App\Models{
 /**
  * 
  *
+ * @property int $id
+ * @property string|null $first_name
+ * @property string|null $last_name
+ * @property string|null $email
+ * @property string|null $phone
+ * @property string|null $location
+ * @property string|null $languages
+ * @property string|null $link
+ * @property string|null $how_did_you_find_us
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
  * @property-read int|null $media_count
  * @method static \Illuminate\Database\Eloquent\Builder|Applicant newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Applicant newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Applicant query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Applicant whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Applicant whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Applicant whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Applicant whereHowDidYouFindUs($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Applicant whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Applicant whereLanguages($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Applicant whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Applicant whereLink($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Applicant whereLocation($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Applicant wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Applicant whereUpdatedAt($value)
  */
 	class Applicant extends \Eloquent implements \Spatie\MediaLibrary\HasMedia {}
 }
---
+++ app/Filament/Resources/ArticleResource.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use AmidEsfahani\FilamentTinyEditor\TinyEditor;
+use App\Filament\Resources\ArticleResource\Pages;
+use App\Models\Article;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ArticleResource extends Resource
+{
+    protected static ?string $model = Article::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                TinyEditor::make('body')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\FileUpload::make('image')
+                    ->image(),
+                Forms\Components\TextInput::make('author')
+                    ->maxLength(255),
+                Forms\Components\DateTimePicker::make('published_at'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('slug')
+                    ->toggleable(isToggledHiddenByDefault: true)
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('author')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('published_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListArticles::route('/'),
+            'create' => Pages\CreateArticle::route('/create'),
+            'edit' => Pages\EditArticle::route('/{record}/edit'),
+        ];
+    }
+}
