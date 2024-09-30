4fc5d1db67bcb0a3c1e21b5ba4f9343f0ccc5125
hn3 <120098784+esghendry@users.noreply.github.com>
2024-09-30 11:37:12 +0200

    changed heading tags from <h4> to <h3> for improved semantic structure in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,7 +89,7 @@
                 <div class="grid mt-12 divide-y divide-rb-dark-blue md:grid-cols-3 md:divide-x md:divide-y-0">
                     <div class="my-6 text-center md:my-auto md:px-6">
                         <img src="{{ asset('assets/icons/lightning.svg') }}" alt="lightning" class="w-10 mx-auto" />
-                        <h4 class="mt-4 text-5xl font-bold">1795 ton*</h4>
+                        <h3 class="mt-4 text-5xl font-bold">1795 ton*</h3>
                         <p class="px-6 mt-2 font-medium">
                             CO&#8322;-uitstoot voorkomen met ons circulaire proces (*en
                             de teller loopt nog!)
@@ -98,7 +98,7 @@
 
                     <div class="my-6 text-center md:my-auto md:px-6">
                         <img src="{{ asset('assets/icons/lightning.svg') }}" alt="lightning" class="w-10 mx-auto" />
-                        <h4 class="mt-4 text-5xl font-bold">11+</h4>
+                        <h3 class="mt-4 text-5xl font-bold">11+</h3>
                         <p class="px-6 mt-2 font-medium">
                             Samenwerkingen met organisaties en marktleiders
                             zoals Accell en Stichting OPEN.
@@ -107,7 +107,7 @@
 
                     <div class="my-6 text-center md:my-auto md:px-6">
                         <img src="{{ asset('assets/icons/lightning.svg') }}" alt="lightning" class="w-10 mx-auto" />
-                        <h4 class="mt-4 text-5xl font-bold">1.196.673+</h4>
+                        <h3 class="mt-4 text-5xl font-bold">1.196.673+</h3>
                         <p class="px-6 mt-2 font-medium">
                             Al meer dan één miljoen cellen kregen bij ons een
                             tweede leven.
