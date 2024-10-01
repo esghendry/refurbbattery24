1aa9abc9eb85a5356e63ebd8321fab780f782268
Giovanni M <g.mierop@esg.works>
2024-10-01 14:20:41 +0200

    add fix for when livewire.js is not loading because of a 404 error

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,7 @@
 import './bootstrap';
 import { gsap } from "gsap";
 import 'preline'
+
+// fix livewire script not loading
+import { Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm'
+Livewire.start()
