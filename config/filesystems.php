fe5767f24397abdcac211094755274d3d52047b2
Giovanni M <g.mierop@esg.works>
2024-10-09 13:39:43 +0200

    add a disk which goes to the same storage as 'do' but with a root folder 'media' for spatie's media-library, to keep it separated.

--- config/filesystems.php
+++ config/filesystems.php
@@ -67,6 +67,17 @@ return [
             'visibility' => 'public',
         ],
 
+        // do media storage for spatie media-library
+        'do_media' => [
+            'driver' => 's3',
+            'key' => env('DO_ACCESS_KEY_ID'),
+            'secret' => env('DO_SECRET_ACCESS_KEY'),
+            'region' => env('DO_DEFAULT_REGION'),
+            'bucket' => env('DO_BUCKET'),
+            'endpoint' => env('DO_ENDPOINT'),
+            'root' => 'media',
+        ],
+
     ],
 
     /*
