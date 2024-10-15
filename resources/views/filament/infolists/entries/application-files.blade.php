71416b62889506d96c1433bb6a6fa99ae09da951
Giovanni M <g.mierop@esg.works>
2024-10-15 17:24:02 +0200

    updated application-files view to work with storageObjects

--- resources/views/filament/infolists/entries/application-files.blade.php
+++ resources/views/filament/infolists/entries/application-files.blade.php
@@ -1,9 +1,38 @@
-<div>
-    @foreach ($getRecord()->getMedia('application_pdf') as $file)
+<x-filament::section>
+    <x-slot name="heading">Uploaded files</x-slot>
 
-        <a href="{{ $file->getTemporaryUrl(now()->addMinutes(5)) }}" target="_blank">
-            {{ $file->name }}
-        </a>
-        
-    @endforeach
-</div>
+    <div class="grid gap-2 py-6">
+        @foreach ($getRecord()->storageObjects as $file)
+            @if ($file->type != "image")
+                <a
+                    href="{{ Storage::disk($file->disk)->temporaryUrl($file->path, now()->addMinutes(5)) }}"
+                    target="_blank"
+                    class="block underline hover:text-blue-500"
+                >
+                    {{ $file->filename }}
+                </a>
+            @endif
+        @endforeach
+    </div>
+
+    <div class="grid gap-2 py-6" style="grid-template-columns: repeat(2, 1fr)">
+        @foreach ($getRecord()->storageObjects as $file)
+            @if ($file->type == "image")
+                <a
+                    href="{{ Storage::disk($file->disk)->temporaryUrl($file->path, now()->addMinutes(5)) }}"
+                    target="_blank"
+                >
+                    <img
+                        src="{{ Storage::disk($file->disk)->temporaryUrl($file->path, now()->addMinutes(5)) }}"
+                        alt="{{ $file->filename }}"
+                        style="
+                            max-width: 500px;
+                            max-height: 500px;
+                            object-fit: contain;
+                        "
+                    />
+                </a>
+            @endif
+        @endforeach
+    </div>
+</x-filament::section>
