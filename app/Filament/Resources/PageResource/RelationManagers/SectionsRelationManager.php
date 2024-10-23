6c3101cb50cb72b083b95a20f7319648443a168e
Giovanni M <g.mierop@esg.works>
2024-10-23 11:01:50 +0200

    wip

--- app/Filament/Resources/PageResource/RelationManagers/SectionsRelationManager.php
+++ app/Filament/Resources/PageResource/RelationManagers/SectionsRelationManager.php
@@ -7,8 +7,6 @@ use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class SectionsRelationManager extends RelationManager
 {
@@ -34,17 +32,20 @@ class SectionsRelationManager extends RelationManager
             ->filters([
                 //
             ])
+            ->reorderable('sectionables.order')
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
+                // Tables\Actions\CreateAction::make(),
+                Tables\Actions\AttachAction::make(),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\DetachAction::make(),
+                // Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                // Tables\Actions\BulkActionGroup::make([
+                //     Tables\Actions\DeleteBulkAction::make(),
+                // ]),
             ]);
     }
 }
